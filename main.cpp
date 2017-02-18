e4394b5a4fb8dbb829a8b857b85e55ea857d1066
Nick McKinney <mckinnnd@uw.edu>
2017-02-18 13:00:25 -0800

    Encoding file to bits.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <map>
 #include <queue>
+#include <stack>
 
 /* Huffman Tuple
  * This is the data. */
@@ -41,21 +42,14 @@ struct node {
         data = d;
     }
 
-    bool operator<(const node &rhs) const {
-        return data->p < rhs.data->p;
-    }
+    bool operator<(const node &rhs) const { return data->p < rhs.data->p; }
 
-    bool operator>(const node &rhs) const {
-        std::cerr << "Hit >" << std::endl;
-        return data->p > rhs.data->p;
-    }
+    bool operator>(const node &rhs) const { return data->p > rhs.data->p; }
 };
 
 /* Custom comparator */
 struct compare_nodes {
-    bool operator()(const node *lhs, const node *rhs) const {
-        return lhs->data->p > rhs->data->p;
-    }
+    bool operator()(const node *lhs, const node *rhs) const { return lhs->data->p > rhs->data->p; }
 };
 
 
@@ -70,13 +64,55 @@ void build_codes(std::string s, node *n, int depth, h_tuple *dict[]) {
         if (n->right)  // We've exhausted all children
             build_codes(s + "1", n->right, depth + 1, dict);
     } else {
-        // h_tuple *t = new h_tuple(depth, n->data->p, s);
         h_tuple *t = new h_tuple;
         t->bits = depth;
         t->p = n->data->p;
         t->code = s;
         dict[(int) n->name] = t;
     }
+}
+
+unsigned long stringToLongBuffer(std::string s, unsigned long buffer) {
+    for (uint b = 0; b < s.length(); b++) {
+        buffer = buffer << 1;
+        int t = s.at(b);
+        if (t == 49)
+            buffer++;
+    }
+
+    return buffer;
+}
+
+uint* bytesToBitsOut(unsigned long buffer, int length, std::ofstream *output) {
+    std::stack<unsigned char> output_stack;
+    uint *ret_val = new uint[2];
+
+    auto offset = (length - (length % 8)) / 8;
+    auto data = buffer >> offset;  // Align our buffer to bytes
+    while (offset > 0) {
+        unsigned char t = (unsigned char) (data & 0xFF);
+        output_stack.push(t);
+        data = data >> 8;
+        offset--;
+    }
+
+    while (!output_stack.empty()) {
+        unsigned char out = output_stack.top();
+        output_stack.pop();
+        *output << out;
+    }
+
+    // Compute remaining data mask
+    uint mask = 0;
+    for (int i = 0; i < (length % 8); i++) {
+        mask = mask << 1;
+        mask++;
+    }
+
+    ret_val[0] = (uint) (buffer & mask);
+    ret_val[1] = (uint) (length % 8);
+
+    return ret_val;
 }
 
 int main(int argc, char *argv[]) {
@@ -97,7 +133,7 @@ int main(int argc, char *argv[]) {
             std::cout << "File I/O failure.  Quitting." << std::endl;
             exit(1);
         }
-        // output_file.open(argv[3]);
+        output_file.open(argv[3]);
     } else {
         print_usage();
         exit(1);
@@ -126,7 +162,6 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // char x = 97;
     // Condense the individual trees into a single tree.
     while (pq.size() > 1) {
         auto l = pq.top();
@@ -134,13 +169,11 @@ int main(int argc, char *argv[]) {
         auto r = pq.top();
         pq.pop();
         h_tuple *h = new h_tuple(l->data->p + r->data->p);
-        // node n{x++, h};
         node *n = new node(0, h);
         n->left = l;
         n->right = r;
         pq.push(n);
     }
-
 
     // Build dictionary
     h_tuple *dictionary[256];
@@ -154,10 +187,46 @@ int main(int argc, char *argv[]) {
             std::cout << (char) i << ":" << dictionary[i]->bits << ":" << dictionary[i]->code << std::endl;
     }
 
-    // Store dictionary to file
+    /* Store dictionary to file */
+    // Store the dictionary preamble
+    for (int i = 0; i < 256; i++)
+        output_file << (char) (dictionary[i]->bits);
 
+    // Store the bits
+    unsigned long buffer = 0;
+    int buf_len = 0;
+    for (int i = 0; i < 256; i++) {
+        std::string c = dictionary[i]->code;
+        buf_len += dictionary[i]-> bits;
+        buffer = stringToLongBuffer(c, buffer);
+        uint *updates = bytesToBitsOut(buffer, buf_len, &output_file);
+        buffer = updates[0];
+        buf_len = updates[1];
+    }
+    // Handle the residual bits
+    if (buf_len > 0) {
+        buffer = buffer << (8 - buf_len);
+        bytesToBitsOut(buffer, 8, &output_file);
+    }
 
-    // Compress
+    // Encode the input file and write to output
+    input_file.open(argv[2]);
+    std::string encoded;
+    buf_len = 0;
+    buffer = 0;
+    while (in = (unsigned char) input_file.get(), input_file.good()) {
+        buf_len += dictionary[in]->bits;
+        buffer = stringToLongBuffer(dictionary[in]->code, buffer);
+        uint *updates = bytesToBitsOut(buffer, buf_len, &output_file);
+        buffer = updates[0];
+        buf_len = updates[1];
+    }
+    // Handle the residual bits
+    if (buf_len > 0) {
+        buffer = buffer << (8 - buf_len);
+        bytesToBitsOut(buffer, 8, &output_file);
+    }
+
 
     return 0;
 }
